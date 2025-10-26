5403342ee9a283fd7c589aac3c39e4ea2b699eb3
YanushkaKumar <141440648+YanushkaKumar@users.noreply.github.com>
2025-10-26 23:58:35 +0530

    new

--- Admin_App/election-admin-app/src/Login.js
+++ Admin_App/election-admin-app/src/Login.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from './firebase';
+import './Login.css';
 
 function Login({ onLogin }) {
   const [email, setEmail] = useState('');
