e7778f2f99a8b4223569baad33af13f4700198c6
YanushkaKumar <141440648+YanushkaKumar@users.noreply.github.com>
2025-12-10 21:03:17 +0530

    push code

--- Admin_App/election-admin-app/src/firebase.js
+++ Admin_App/election-admin-app/src/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP9LtIW-qwDR5lp6U808v7Ivj2uC368s0",
-  authDomain: "electionnew-app.firebaseapp.com",
-  databaseURL: "https://electionnew-app-default-rtdb.firebaseio.com",
-  projectId: "electionnew-app",
-  storageBucket: "electionnew-app.firebasestorage.app",
-  messagingSenderId: "350685026683",
-  appId: "1:350685026683:web:7569e3d262f838430985de",
-  measurementId: "G-VY03SSL34X"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
